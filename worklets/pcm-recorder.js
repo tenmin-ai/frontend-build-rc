4149d4c7504f0d5dda5e532e15fe37296bfc95a3
JoongWonSeo <JoongWonSeo@users.noreply.github.com>
2024-12-17 11:55:45 +0000

    deploy: tenmin-ai/tenmin@ded673303c9d239dbecd0f7828e2cdec79d61e13

---
+++ worklets/pcm-recorder.js
@@ -0,0 +1,55 @@
+class PCMRecorder extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.BUFFER_SIZE = 3200;
+    this.int16Buffer = new Int16Array(this.BUFFER_SIZE);
+    this.bufferOffset = 0;
+    console.log("PCMRecorder initialized");
+  }
+
+  process(inputs, outputs, parameters) {
+    const input = inputs[0];
+    if (!input) {
+      console.warn("No input data:");
+      return true;
+    }
+
+    const channelData = input[0];
+    if (!channelData) {
+      // console.warn("No channel data");
+      return true;
+    }
+
+    for (let i = 0; i < channelData.length; i++) {
+      const sample = Math.max(-1, Math.min(1, channelData[i]));
+      const intSample = sample < 0 ? sample * 32768 : sample * 32767;
+
+      // Store the sample
+      this.int16Buffer[this.bufferOffset++] = intSample;
+
+      // If we've filled the buffer, send it out
+      if (this.bufferOffset === this.BUFFER_SIZE) {
+        // Post data to main thread
+        this.port.postMessage(this.int16Buffer.buffer, [
+          this.int16Buffer.buffer,
+        ]);
+
+        // Reallocate a new buffer since we've transferred the old one
+        this.int16Buffer = new Int16Array(this.BUFFER_SIZE);
+        this.bufferOffset = 0;
+      }
+    }
+
+    // If you want to continue the audio flow, copy input to output
+    for (let i = 0; i < outputs.length; ++i) {
+      const output = outputs[i];
+      for (let channel = 0; channel < output.length; ++channel) {
+        output[channel].set(input[channel]);
+      }
+    }
+
+    return true;
+  }
+}
+
+registerProcessor("pcm-recorder", PCMRecorder);
