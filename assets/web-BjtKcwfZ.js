4b5eee0d06e3868bcff9fc58ec5bbc9864d3a920
tiramisuuuu <tiramisuuuu@users.noreply.github.com>
2025-03-06 19:06:11 +0000

    deploy: tenmin-ai/tenmin@861caf2f6f938ade80d9e060ea5957dd0372ab6d

---
+++ assets/web-BjtKcwfZ.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-A_q9ZIDP.js";class r extends n{async enable(e){}async disable(e){}}export{r as SafeAreaWeb};
--- assets/web-DnASs7tU.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-B8AY3r6v.js";class r extends n{async enable(e){}async disable(e){}}export{r as SafeAreaWeb};
