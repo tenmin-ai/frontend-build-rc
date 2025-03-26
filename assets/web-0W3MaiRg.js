9b34cc650fa996ade40c716f63191404be3e1a6c
JoongWonSeo <JoongWonSeo@users.noreply.github.com>
2025-03-26 21:51:00 +0000

    deploy: tenmin-ai/tenmin@a09e5af4080cd5646ea39b0b1c920893fc9137f1

--- assets/web-0W3MaiRg.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-BMjB2S_T.js";class r extends n{async enable(e){}async disable(e){}}export{r as SafeAreaWeb};
---
+++ assets/web-DR8m5gZZ.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-DSXN6T0y.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
